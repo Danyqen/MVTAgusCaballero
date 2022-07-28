7df8c6f24ef68de3c0f1a23663e4fddda773c8fb
Agustin Caballero <aguscaballero117@gmail.com>
2022-07-28 02:18:07 -0300

    hasta la clase 22

--- mascota/models.py
+++ mascota/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Gato(models.Model):
-    Apodo = models.CharField(max_length=30)
+    apodo = models.CharField(max_length=30)
     edad = models.IntegerField()
     fecha_creacion = models.DateField(null=True)
     
     def __str__(self):
-        return f'Soy un gato llamado Sr.{self.nombre}'   
+        return f'Soy un gato llamado Sr.{self.apodo}'
--- mascota/views.py
+++ mascota/views.py
@@ -1,7 +1,7 @@
 from .models import Gato
 from django.views.generic.list import ListView
-from django.views.generic.list import DeleteView, UpdateView, CreateView
-# from django.views.generic.list import View
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from django.views.generic import DetailView
 
 
 class ListadoGatos(ListView):
@@ -11,22 +11,24 @@ class ListadoGatos(ListView):
     
 class CrearGato(CreateView):
     model=Gato
-    template_name = 'Gato/crear_gatos.html'
-    success_url = 'gatos'
-    
+    template_name = 'Gato/crear_gato.html'
+    success_url = '/mascotas/gatos'
+    fields = ['apodo', 'edad', 'fecha_creacion']
+
     
 class EditarGato(UpdateView):
     model=Gato
-    template_name = 'Gato/editar_gatos.html'
-    success_url = 'gatos'
-    success_url = 'gatos'
+    template_name = 'Gato/editar_gato.html'
+    success_url = '/mascotas/gatos'
+    fields = ['apodo', 'edad', 'fecha_creacion']
     
     
 class EliminarGato(DeleteView):
     model=Gato
-    template_name = 'Gato/eliminar_gatos.html'
-    success_url = 'gatos'
+    template_name = 'Gato/eliminar_gato.html'
+    success_url = '/mascotas/gatos'
     
     
-# class MostrarGato(View):
-#     ...
+class MostrarGato(DetailView):
+    model=Gato
+    template_name = 'Gato/mostrar_gato.html'
