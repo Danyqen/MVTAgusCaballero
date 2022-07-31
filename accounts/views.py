ef8b4c7beea9b99a50b0fd071742c0151fce0a97
Agustin Caballero <aguscaballero117@gmail.com>
2022-07-31 05:50:00 -0300

    Hasta clase 24

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
-from modulefinder import IMPORT_NAME
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login as django_login
+from django.contrib.auth.decorators import login_required
 
-from .form import MyUserCreationForm
+from accounts.models import MasDatosUsuario
+from .form import MyUserCreationForm, MyUserEditForm
 
 
 def login(request):
@@ -48,3 +48,46 @@ def register(request):
     
     form = MyUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+@login_required
+def perfil(request):
+    return render(request, 'accounts/perfil.html')
+
+
+@login_required
+def editar_perfil(request):
+    
+    user = request.user
+    mas_datos_usuario, _ = MasDatosUsuario.objects.get_or_create(user=user)
+    
+    if request.method == 'POST':
+        form = MyUserEditForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+            user.first_name = data.get('first_name') if data.get('first_name') else user.first_name
+            user.last_name = data.get('last_name') if data.get('last_name') else user.last_name
+            user.email = data.get('email') if data.get('email') else user.email
+            mas_datos_usuario.avatar = data.get('avatar') if data.get('avatar') else mas_datos_usuario.avatar
+            
+            if data.get('password1') and data.get('password1') == data.get('password2'):
+                user.set_password(data.get('password1'))
+            
+            mas_datos_usuario.save()
+            user.save()
+            
+            return redirect('perfil')
+        
+        else:
+            return render(request, 'accounts/editar_perfil.html', {'form', form})
+        
+    form = MyUserEditForm(
+        initial={
+            'email': user.email,
+            'fist_name': user.first_name,
+            'last_name': user.last_name,
+            'avatar': mas_datos_usuario.avatar
+        }
+    )
+    
+    return render(request, 'accounts/editar_perfil.html', {'form': form})
